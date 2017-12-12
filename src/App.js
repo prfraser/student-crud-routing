2a0b5bd784ac630ad75e5806ca8620ea0810b31f
philip fraser <pr.fraser3@gmail.com>
2017-12-12 11:55:54 +1000

    initialized app. included react router

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { 
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect,
+  Switch
+  } from 'react-router-dom';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      </div>
+    );
+  }
+}
+
+export default App;
